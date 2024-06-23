e8d3dc3608293baf8b22cade1fbafe16f9620c3b
VBelyaev25 <38358503+VBelyaev25@users.noreply.github.com>
2024-06-23 11:59:31 +0300

    Delete src directory

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-# # Define the __all__ variable
-# __all__ = ["circle", "figure", "rectangle", "square", "triangle"]
-#
-# # Import the submodules
-# import circle
-# import figure
-# import rectangle
-# import square
-# import triangle
